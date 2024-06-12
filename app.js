5b7330f98f1ed481e0f3abece1508d09305fc6f6
mvr2004 <165604764+mvr2004@users.noreply.github.com>
2024-06-12 19:06:18 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,39 +1,24 @@
+// server.js
 const express = require('express');
-const sql = require('mssql');
-
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const sql = require('./db');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
-// Configuração do SQL Server
-const dbConfig = {
-    user: 'seu_usuario',
-    password: 'sua_senha',
-    server: 'seu_servidor',
-    database: 'sua_base_de_dados',
-    options: {
-        encrypt: true, // Use este campo se você estiver utilizando o Azure
-        trustServerCertificate: true, // Use este campo se você estiver utilizando um SQL Server local
+app.use(bodyParser.json());
+app.use(cors());
+
+// Exemplo de rota para obter todos os usuários
+app.get('/users', async (req, res) => {
+    try {
+        const result = await sql.query`SELECT * FROM TP_UTILIZADORES`;
+        res.json(result.recordset);
+    } catch (err) {
+        res.status(500).send(err.message);
     }
-};
-
-sql.connect(dbConfig).then(pool => {
-    if (pool.connected) {
-        console.log('Conectado ao SQL Server');
-    }
-
-    app.get('/dados', async (req, res) => {
-        try {
-            const result = await pool.request().query('SELECT * FROM sua_tabela');
-            res.json(result.recordset);
-        } catch (err) {
-            res.status(500).send(err.message);
-        }
-    });
-
-}).catch(err => {
-    console.log('Erro ao conectar ao SQL Server:', err);
 });
 
 app.listen(port, () => {
-    console.log(`Servidor rodando na porta ${port}`);
+    console.log(`Server running at http://localhost:${port}`);
 });
